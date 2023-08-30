b0d641cbfe016630c3b855c2750ef8814d9a9ec7
Asad Khan <asad@macworldtechnology.com>
2023-08-30 01:48:24 -0700

    vercel 2

--- index.js
+++ index.js
@@ -4,18 +4,11 @@ const express = require('express');
 const cors = require('cors');
 const users = require('./routes/users');
 const trade = require('./routes/trade')
-const morgan = require('morgan')
 const app = express();
-const yaml = require('yamljs')
-const swaggerJsdoc = yaml.load('./config/swagger.yaml')
-const swaggerUI = require('swagger-ui-express')
-const {errorHandler} = require('./handler/errorHandler')
 
 
 // app.use('/api/docs',swaggerUI.serve,swaggerUI.setup(swaggerJsdoc))
-app.set('view engine','ejs')
-app.use(morgan('dev'))
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors('*'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -25,7 +18,5 @@ app.get('/', (req, res) => {
 app.use('/users', users);
 app.use('/trade', trade);
 
-app.use(errorHandler)
-console.log(process.env.WEBSITE_LINK)
 const port = process.env.PORT || 3005;
 app.listen(port, () => console.log(`Server running on port ${port}`));
